3e08d1c6d850034e553e0f41165aef620470ab5c
GuztaJF-DS <noobshippuden13@gmail.com>
2021-07-08 17:15:23 -0300

    Added Update Products Feature

--- src/controllers/productServicesControl.js
+++ src/controllers/productServicesControl.js
@@ -66,6 +66,36 @@ router.get('/GetOne',async(req,res)=>{
     }catch(err){
         res.status(400).send({error:'Error'});
     }
+});
+
+router.put('/Update',async(req,res)=>{
+    try{
+        const result=await ProSer.findOne({
+            where:{
+                [Op.and]:[{id:req.body.id},{userId:req.body.userId}]
+            }
+        });
+        if(result){
+            const Variables='{"vars":["Code","Name","Description","Type","Value","TotalAmount"]}';
+            const obj=JSON.parse(Variables);
+
+            for(var x=0;x<obj.vars.length;x++){
+                const str=obj.vars[x];
+                if(req.body[str]!=null){
+                    result[str]=req.body[str];
+                    await result.save();
+                    console.log("1");
+                }else{
+                    console.log("0");
+                }
+            }
+            
+            res.json({menssage:"Values Changed"});
+        }
+    }catch(err){
+        console.log(err);
+        res.status(400).send({error:'error'});
+    }
 })
 
 module.exports=app=>app.use("/products", router);
